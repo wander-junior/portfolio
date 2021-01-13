9aa989e1dd14a80b75ff130da7323a9a25a62b3a
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-01-13 19:53:58 -0300

    Create Menu component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Menu from "./components/Menu";
+
+
 function App() {
   return (
     <div className="App">
-
+      <Menu />
     </div>
   );
 }
---
+++ src/components/Menu/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Menu() {
+    return (
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <li>SOBRE</li>
+                <li>PORTFÓLIO</li>
+                <li>CONTATO</li>
+            </ul>
+        </nav>
+    )
+}
