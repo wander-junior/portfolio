33a0f2e257a5b1e7ea4336113557ba8af437a7ea
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-01-22 17:55:32 -0300

    Define theme colors

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import Title from "./components/Title";
 import Menu from "./components/Menu";
 import Abilities from "./components/Abilities";
 
+const theme = {
+  colors: {
+    primary: '#945FF5',
+    secondary: '#5B21CD',
+    tertiary: '#FEFDFE',
+    quartenary: '#211D2F'
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <Menu />
+      <Menu/>
       <Title />
       <Abilities />
     </div>
