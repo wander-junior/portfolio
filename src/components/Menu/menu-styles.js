acce47f33d3613ed85e72c0ec3e29adff70c8555
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-01-27 18:51:01 -0300

    Defines typography size for menu

--- src/components/Menu/menu-styles.js
+++ src/components/Menu/menu-styles.js
@@ -2,20 +2,20 @@ import styled from 'styled-components'
 import styles from '../../styles.js';
 
 const MenuWrapper = styled.nav`
-background: ${styles.theme.colors.primary};
-
+    background: ${styles.theme.colors.primary};
 `;
 
 const MenuUl = styled.ul`
-display: flex;
-${styles.theme.containerConfigs}
+    display: flex;
+    ${styles.theme.containerConfigs}
 `;
 
 const MenuItem = styled.li`
-color: ${styles.theme.colors.tertiary};
-margin-right: 30px;
-padding-top: 40px;
-padding-bottom: 40px;
+    color: ${styles.theme.colors.tertiary};
+    margin-right: 30px;
+    padding-top: 40px;
+    padding-bottom: 40px;
+    font-size: ${styles.theme.typoSizes.medium};
 `;
 
 export {
