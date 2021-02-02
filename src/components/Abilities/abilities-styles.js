22216584b712bf6c8f208266c655f2d428fdf87e
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-02-02 19:48:20 -0300

    Create Container styled-component to abilities

--- src/components/Abilities/abilities-styles.js
+++ src/components/Abilities/abilities-styles.js
@@ -1,13 +1,17 @@
-import styles from '../../styles.js';
+import { colors, containerConfigs } from '../../styles.js';
 import styled from 'styled-components';
 
 const AbilitiesWrapper = styled.div`
-    background: ${styles.theme.colors.primary};
-    color: ${styles.theme.colors.tertiary};
+    background: ${colors.primary};
+    color: ${colors.tertiary};
 `;
 
 const Title = styled.h1`
     padding-top: 60px;
 `;
 
-export {AbilitiesWrapper, Title};
+const Container = styled.div`
+    ${containerConfigs};
+`;
+
+export {AbilitiesWrapper, Title, Container};
--- src/components/Abilities/index.js
+++ src/components/Abilities/index.js
@@ -2,31 +2,33 @@ import React from 'react'
 import frontEndIcon from '../../assets/img/frontend.svg';
 import backEndIcon from '../../assets/img/backend.svg';
 import uiuxIcon from '../../assets/img/ui.svg';
-import { AbilitiesWrapper, Title } from './abilities-styles';
+import { AbilitiesWrapper, Title, Container } from './abilities-styles';
 
 export default function Abilities() {
 
     return (
         <AbilitiesWrapper>
-            <Title>Serviços</Title>
-            <ul>
-                <li>
-                    <img src={frontEndIcon} alt={"Ícone Front End"}/>
-                    <h2>Front End</h2>
-                    <p>Eu programo a interface gráfica de seu site</p>
-                </li>
-                <li>
-                    <img src={backEndIcon} alt={"Ícone Back End"}/>
-                    <h2>Back End</h2>
-                    <p>Eu implemento aplicações e o banco de dados do seu site</p>
-                </li>
-                <li>
-                    <img src={uiuxIcon} alt={"Ícone UI/UX Design"}/>
-                    <h2>UI/UX Design</h2>
-                    <p>Eu crio interfaces intuitivas e bonitas para o seu site</p>
-                </li>
-            </ul>
-            <button>Saiba mais</button>
+            <Container>
+                <Title>Serviços</Title>
+                <ul>
+                    <li>
+                        <img src={frontEndIcon} alt={"Ícone Front End"}/>
+                        <h2>Front End</h2>
+                        <p>Eu programo a interface gráfica de seu site</p>
+                    </li>
+                    <li>
+                        <img src={backEndIcon} alt={"Ícone Back End"}/>
+                        <h2>Back End</h2>
+                        <p>Eu implemento aplicações e o banco de dados do seu site</p>
+                    </li>
+                    <li>
+                        <img src={uiuxIcon} alt={"Ícone UI/UX Design"}/>
+                        <h2>UI/UX Design</h2>
+                        <p>Eu crio interfaces intuitivas e bonitas para o seu site</p>
+                    </li>
+                </ul>
+                <button>Saiba mais</button>
+            </Container>
         </AbilitiesWrapper>
     )
 }
