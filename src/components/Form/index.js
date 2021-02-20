30219692a96a504a0c0eb82a5e5c888ee7a98db7
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-02-20 20:08:55 -0300

    Create State Hooks in form and install axios

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,21 +3,35 @@ import Button from '../Button';
 
 import { Contact, LabelTxt, Input, InputTxtArea } from './form-styles';
 
-export default function index() {
+export default function Form() {
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [message, setMessage] = React.useState('');
+
+    const handleChange = (e) => {
+        if (e.target.name === 'name') setName (e.target.value);
+        else if (e.target.name === 'email') setEmail (e.target.value);
+        else if (e.target.name === 'message') setMessage (e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <Contact>
                 <label>
                     <LabelTxt>Nome</LabelTxt>
-                    <Input type="text" name="name" />
+                    <Input type="text" name="name" value={name} onChange={handleChange}/>
                 </label>
                 <label>
                     <LabelTxt>E-mail</LabelTxt>
-                    <Input type="email" name="email" />
+                    <Input type="email" name="email" value={email} onChange={handleChange}/>
                 </label>
                 <label>
                     <LabelTxt>Mensagem</LabelTxt>
-                    <InputTxtArea name="message"/>
+                    <InputTxtArea name="message" value={message} onChange={handleChange}/>
                 </label>
                 <Button text="Enviar" mainColor="#5B21CD" secondaryColor="#FEFDFE"/>
             </Contact>
