caca3781143c5a45b3338e0ab23f983187704115
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-02-23 16:44:29 -0300

    Stylize the modal's inner content

---
+++ src/components/Portfolio/PortfolioItem/PortfolioItem-styles.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+import { colors, typoSizes } from '../../../styles';
+
+const ItemImg = styled.img`
+    :hover {
+        cursor: pointer; 
+    }
+`;
+
+const Modal = styled.div`
+    display: ${props => props.isModalOpen ? 'block' : 'none'};
+    position: absolute;
+    width: 1160px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    background-color: ${colors.tertiary};
+    color: ${colors.quartenary};
+`;
+
+const ContentWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 40px 80px;
+    grid-gap: 20px;
+`;
+
+const ModalImg = styled.img`
+    max-width: 490px;
+`;
+
+const ModalTitle = styled.h2`
+    font-size: ${typoSizes.medium};
+    padding-bottom: 20px;
+`;
+
+const ModalText = styled.p`
+    font-size: ${typoSizes.small};
+    line-height: ${typoSizes.smallLineHeight};
+    padding-bottom: 20px;
+`;
+
+const LinkType = styled.p`
+    font-size: ${typoSizes.small};
+    font-weight: bold;
+`;
+
+const ModalLink = styled.a`
+    font-size: ${typoSizes.small};
+    color: ${colors.primary};
+    :hover {
+        color: ${colors.secondary};
+    }
+`;
+
+const CloseButton = styled.button`
+    width: 40px;
+    height: 40px;
+    background-color: ${colors.tertiary};
+    border: 3px solid ${colors.quartenary};
+    border-radius: 9px;
+    color: ${colors.quartenary};
+    font-size: ${typoSizes.medium};
+    position: absolute;
+    left: calc(100% - 60px);
+    top: 20px;
+    :hover {
+        border-color: ${colors.secondary};
+        color: ${colors.secondary};
+        cursor: pointer;
+    }
+`;
+
+export { 
+    ItemImg, 
+    Modal, 
+    ModalImg, 
+    ModalTitle, 
+    ModalText, 
+    LinkType, 
+    ModalLink, 
+    ContentWrapper, 
+    CloseButton 
+};
--- src/components/Portfolio/PortfolioItem/index.js
+++ src/components/Portfolio/PortfolioItem/index.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import ItemImage from '../../../assets/img/ItemPortfolio1.svg'
+import React from 'react';
+import ItemImage from '../../../assets/img/ItemPortfolio1.svg';
+import PrintItem from '../../../assets/img/ItemPrint1.png';
+
+import { 
+    ItemImg, 
+    Modal, 
+    ModalImg, 
+    ModalTitle, 
+    ModalText, 
+    LinkType, 
+    ModalLink, 
+    ContentWrapper,
+    CloseButton
+} from './PortfolioItem-styles';
 
 export default function PortfolioItem() {
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+    const handleImageClick = () => {
+        setIsModalOpen(true);
+    }
+
+    const handleCloseClick = () => {
+        setIsModalOpen(false)
+    }
+
     return (
         <div>
-            <img src={ItemImage} alt=""></img>
+            <Modal isModalOpen={isModalOpen}>
+                <CloseButton onClick={handleCloseClick}>X</CloseButton>
+                <ContentWrapper>
+                    <div>
+                        <ModalImg src={PrintItem}/>
+                    </div>
+                    <div>
+                        <ModalTitle>Controle Financeiro Pessoal</ModalTitle>
+                        <ModalText>Projeto desenvolvido durante o Bootcamp fullstack da IGTI. O projeto consiste de um controle financeiro onde pode se adicionar, apagar, editar e deletar registros. No  Front End foi utilizado React e na estilização foi utilizado CSS Flexbox com Materialize. O Back End foi desenvolvido em Node.js, o banco de dados utilizado foi o MongoDB e o deploy foi feito no Heroku.</ModalText>
+                        <LinkType>Link:</LinkType>
+                        <ModalLink href="https://controle-financeiro-wander.herokuapp.com/">https://controle-financeiro-wander.herokuapp.com/</ModalLink>
+                        <LinkType>GitHub:</LinkType>
+                        <ModalLink href="https://github.com/wander-junior/Controle-Financeiro-Pessoal-IGTI">https://github.com/wander-junior/Controle-Financeiro-Pessoal-IGTI</ModalLink>
+                    </div>
+                </ContentWrapper>
+            </Modal>
+            <ItemImg src={ItemImage} alt="" onClick={handleImageClick} />
         </div>
     )
 }
