0fbf96af44565e770c44849f375d90fc05c407de
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-02-24 19:26:30 -0300

    Make modal responsive

--- src/components/Portfolio/PortfolioItem/PortfolioItem-styles.js
+++ src/components/Portfolio/PortfolioItem/PortfolioItem-styles.js
@@ -10,15 +10,26 @@ const ItemImg = styled.img`
 const Modal = styled.div`
     display: ${props => props.isModalOpen ? 'block' : 'none'};
     position: fixed;
-    width: 1160px;
-    height: 720px;
+    max-width: 1160px;
+    max-height: 90vh;
     top: 50%;
     left: 50%;
-    margin-top: -360px;
+    margin-top: -45vh;
     margin-left: -580px;
+    overflow: auto;
     background-color: ${colors.tertiary};
     color: ${colors.quartenary};
     z-index: 2;
+
+    @media (max-width: 700px) {
+        width: 460px;
+        margin-left: -230px;
+    }
+
+    @media (max-width: 500px) {
+        width: 90vw;
+        margin-left: -45vw;
+    }
 `;
 
 const ModalBackground = styled.div`
@@ -35,10 +46,21 @@ const ContentWrapper = styled.div`
     grid-template-columns: 1fr 1fr;
     padding: 40px 80px;
     grid-gap: 20px;
+
+    @media (max-width: 700px) {
+        grid-template-columns: 1fr;
+        padding: 100px 40px;
+    }
 `;
 
 const ModalImg = styled.img`
     max-width: 490px;
+    @media (max-width: 700px) {
+        width: 385px;
+    }
+    @media (max-width: 500px) {
+        width: 80vw;
+    }
 `;
 
 const ModalTitle = styled.h2`
