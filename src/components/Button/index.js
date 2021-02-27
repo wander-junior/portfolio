343ab328558ca7cf63b64b5fed1b86a43470e4e3
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-02-26 22:40:02 -0300

    Transfer style to outside the button function

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,20 +3,21 @@ import styled from 'styled-components';
 
 import {typoSizes} from './../../styles';
 
+const StyledButton = styled.button`
+    font-size: ${typoSizes.medium};
+    background-color: ${props => props.secondaryColor};
+    color: ${props => props.mainColor};
+    border: 3px solid ${props => props.mainColor};
+    border-radius: 9px;
+    width: 170px;
+    height: 60px;
+`;
+
 export default function index({text, mainColor, secondaryColor}) {
 
-    const StyledButton = styled.button`
-        font-size: ${typoSizes.medium};
-        background-color: ${secondaryColor};
-        color: ${mainColor};
-        border: 3px solid ${mainColor};
-        border-radius: 9px;
-        width: 170px;
-        height: 60px;
-    `;
 
     return (
-        <StyledButton>
+        <StyledButton mainColor={mainColor} secondaryColor={secondaryColor}>
             {text}
         </StyledButton>
     )
