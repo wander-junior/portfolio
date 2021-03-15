9ef3dbd5205a0d932997cfb036944d67de034031
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-03-15 16:33:39 -0300

    Fix accentuation issue with headerImgs

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -3,7 +3,7 @@ import { HeaderWrapper } from '../PageHeader/pageHeader-styles';
 
 export default function index({name, text}) {
     return (
-        <HeaderWrapper name={name}>
+        <HeaderWrapper name={name.normalize('NFD').replace(/[\u0300-\u036f]/g, "")}>
             <h1>{name}</h1>
             <h2>{text}</h2>
         </HeaderWrapper>
